8860e9b74193895033e850c2c7a77c39c5e13088
Ivy <tarraconeros@gmail.com>
2020-10-21 19:00:58 +0200

    Eliminado código innecesario (pre-API).

--- app/src/main/java/com/isweather/MainActivity.kt
+++ app/src/main/java/com/isweather/MainActivity.kt
@@ -38,35 +38,6 @@ class MainActivity : AppCompatActivity() {
             //mostrar mensaje
             Toast.makeText(this,"No hay internet", Toast.LENGTH_SHORT).show()
         }
-
-        /*
-        val ciudadtgn = Ciudad("Ciudad de Tarragona", 17,"Soleado" )
-        val ciudadbcn = Ciudad("Ciudad de Barcelona", 16,"Nublado")
-        val ciudadmad = Ciudad("Ciudad de Madrid", 13,"Lluvia")
-
-        if(ciudad == "ciudad-tarragona"){
-            // mostrar info de la ciudad de tgn
-            tvCiudad?.text = ciudadtgn.nombre
-            tvGrados?.text = ciudadtgn.grados.toString() + "º"
-            tvEstatus?.text = ciudadtgn.estatus
-
-        }else if (ciudad == "ciudad-barcelona"){
-            // mostrar info de la ciudad de bcn
-            tvCiudad?.text = ciudadbcn.nombre
-            tvGrados?.text = ciudadbcn.grados.toString() + "º"
-            tvEstatus?.text = ciudadbcn.estatus
-        }
-        else if (ciudad == "ciudad-madrid") {
-            // mostrar info de la ciudad de madrid
-            tvCiudad?.text = ciudadmad.nombre
-            tvGrados?.text = ciudadmad.grados.toString() + "º"
-            tvEstatus?.text = ciudadmad.estatus
-
-        }else{
-            Toast.makeText(this,"No se encuentra la información.", Toast.LENGTH_SHORT).show()
-    }
-
-         */
 }
 
         //Método para Volley
